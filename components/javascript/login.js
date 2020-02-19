9685166b508446cc9cb77bd5c773477c4cfe0841
Spencer Fitch <SpencerFitch2022@u.northwestern.edu>
2020-02-19 15:01:31 -0600

    Added features to login page and moved javascript code
    - Added option to switch between login and signup within the window
    - Added confirm password parameter to the sign up page
    - Moved click handlers for header buttons to login.js

--- components/javascript/login.js
+++ components/javascript/login.js
@@ -2,14 +2,55 @@
 // Default activeUser is a guest with undecided major on startup
 var activeUser = {username: "Guest", password: "", major: "Undecided", classes: []};
 
+// Array for holding the users registered with the website
 let users = [
   {username: "willie", password: "wildcat", major: "Computer Science", classes: ["Comp_Sci 111", "Comp_Sci 211", "Comp_Sci212", "Comp_Sci213", "Comp_Sci 214", "Comp_Sci 330"]},
   {username: "morty", password: "wildcat", major: "Econonics", classes: ["Econ 201", "Econ 202", "Econ 281", "Econ 310-1", "Econ 310-2", "Econ 311"]}
 ];
 
-function loginFields() {
+
+///// Onclick actions for the header buttons ///// 
+function displayLogin() {
+  document.getElementById('conf-psw-tag').style.display='none';
+  document.getElementById('conf-psw-field').style.display='none';
+  document.getElementById('login-button').style.display='block';
+  document.getElementById('switch_to_signup').style.display='block';
+  document.getElementById('createaccount-button').style.display='none';
+  document.getElementById('switch_to_login').style.display='none';
+  document.getElementById('id01').style.display='block';
+}
+
+function displaySignUp() {
+  document.getElementById('conf-psw-tag').style.display='block';
+  document.getElementById('conf-psw-field').style.display='block';
+  document.getElementById('login-button').style.display='none';
+  document.getElementById('switch_to_signup').style.display='none';
+  document.getElementById('createaccount-button').style.display='block';
+  document.getElementById('switch_to_login').style.display='block';
+  document.getElementById('id01').style.display='block';
+}
+
+function logout() {
+  // Prompt user to confirm logout
+  if (confirm("Are you sure you want to log out?")){
+    // Reset active user to guest and reset the page
+    activeUser = {username: "Guest", password: "", major: "Undecided", classes: []};
+    document.getElementById("greeting").innerHTML = activeUser.username;
+    updateClasses();
+    document.getElementById("signup").style.display = "flex";
+    document.getElementById("login").style.display = "flex";
+    document.getElementById("logout").style.display = "none";
+  }
+  return;
+}
+/////
+
+
+///// Handles login process /////
+function readLoginFields() {
   // Read information from login window fields
   var loginData = document.getElementById("userlogin");
+  console.log(loginData.elements[2].value);
   tryLogin(loginData.elements[0].value, loginData.elements[1].value);
   return;
 }
@@ -37,11 +78,14 @@ function tryLogin(name, pass) {
     }
   }
   // Iterated over all users and no user with this username found
-  alert("Username not found. Please create an account.");
+  alert("Username not found. Please sign up and create an account to continue.");
   resetForm(1);
   return;
 }
+/////
 
+
+///// Handle making account ////
 function makeAccount() {
   var loginData = document.getElementById("userlogin");
   newUser = {username: loginData.elements[0].value, password: loginData.elements[1].value, major:'Undecided', classes:[]};
@@ -54,7 +98,13 @@ function makeAccount() {
       return;
     } 
   }
-  // Username not taken so add account
+  if (newUser.password != loginData.elements[2].value) {
+    // This phrasing is abiguous and will be changed later
+    alert('Passwords must match to create an account');
+    resetForm(1);
+    return;
+  }
+  // Username not taken and passwords match so add account
   users.push[newUser];
   document.getElementById("greeting").innerHTML = newUser.username;
   activeUser = newUser;
@@ -64,30 +114,32 @@ function makeAccount() {
   updateClasses();
   document.getElementById('id01').style.display = "none";
 }
+/////
 
+
+///// Clears out the login form based on resetCode set by programmer /////
 function resetForm(resetCode) {
   var logindata = document.getElementById("userlogin");
-  // resetCode passed in with resetForm to determine
-  // which fields of the form to reset
   switch (resetCode){
     case 0:
       // Reset username and password fields
       logindata.elements[0].value = '';
     case 1:
-      // Reset just the password field
+      // Reset just the password fields
       logindata.elements[1].value = '';
+      logindata.elements[2].value = '';
       return;
     default:
       // log invalid resetCode input and return
       console.log('#resetForm: Invalid resetCode: ' + resetCode);
       return;
   }
-  
 }
+/////
 
 
+///// Updates the classes displayed on the sidebar based on activeUser /////
 function updateClasses() {
-  // Update displayed information when a user logs in or signs up
   document.getElementById('major-header').innerHTML = activeUser.major;
   if (activeUser.major == "Undecided"){
     document.getElementById('class-container').innerHTML = ``;
@@ -99,17 +151,4 @@ function updateClasses() {
     }
   } 
 }
-
-function logout() {
-  // Prompt user to confirm logout
-  if (confirm("Are you sure you want to log out?")){
-    // Reset active user to guest and reset the page
-    activeUser = {username: "Guest", password: "", major: "Undecided", classes: []};
-    document.getElementById("greeting").innerHTML = activeUser.username;
-    updateClasses();
-    document.getElementById("signup").style.display = "flex";
-    document.getElementById("login").style.display = "flex";
-    document.getElementById("logout").style.display = "none";
-  }
-  return;
-}
+/////
