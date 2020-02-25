66c74e5039f86de71123320b54bd48479409da0b
caedonhsieh <caedonhsieh@dhcp-10-105-92-112.wireless.northwestern.private>
2020-02-24 22:19:13 -0600

    test-fetch-courses

--- components/javascript/class_search.js
+++ components/javascript/class_search.js
@@ -1,1 +1,15 @@
-const baseURL = "http://api.asg.northwestern.edu/terms/?key=eQSCJbgt58PVr9KC"
+const baseURL = "http://api.asg.northwestern.edu/courses/?key=eQSCJbgt58PVr9KC"
+
+const getCourses = (paramsDict) => {
+    let endpoints = "";
+    for (var key in paramsDict) {
+        endpoints += `&${key}=${paramsDict[key]}`
+    }
+    fetch(baseURL + endpoints, { mode: 'no-cors' })
+        .then(response => response.json())
+        .then(showCourses);
+}
+
+const showCourses = (response) => {
+    console.log(response);
+}
